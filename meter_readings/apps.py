57f055d71a5d1679531bdc91f2ca9018449121dc
mxcode1 <mxdevelopment@yahoo.com>
2025-12-18 10:15:00 +0100

    Create meter_readings Django app
    Application structure:
    - AppConfig with verbose name
    - Package initialization
    - Ready for models and views

---
+++ meter_readings/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MeterReadingsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "meter_readings"
+    verbose_name = "Meter Readings"
