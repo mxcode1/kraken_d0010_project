3c2968ffd9cf48dfcee0d8567e56e2113c339921
mxcode1 <mxdevelopment@yahoo.com>
2025-12-18 16:45:00 +0100

    Add security settings (HSTS, secure cookies)
    Production security (DEBUG=False):
    - SSL redirect
    - Secure cookies
    - HSTS with 1-year max-age
    - XSS protection
    - Clickjacking prevention

--- config/settings.py
+++ config/settings.py
@@ -85,3 +85,15 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security Settings (Production only)
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
