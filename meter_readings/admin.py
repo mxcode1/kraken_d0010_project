3e48e9562b2c81e3ffd69a73f51452ffe287f327
mxcode1 <mxdevelopment@yahoo.com>
2025-12-20 08:30:00 +0100

    Add custom admin list displays
    Enhanced admin interface:
    - list_display for key fields
    - Date hierarchy for readings
    - Foreign key display in lists
    - Professional admin UX

--- meter_readings/admin.py
+++ meter_readings/admin.py
@@ -2,7 +2,31 @@ from django.contrib import admin
 from .models import FlowFile, MeterPoint, Meter, Reading
 
 
-admin.site.register(FlowFile)
-admin.site.register(MeterPoint)
-admin.site.register(Meter)
-admin.site.register(Reading)
+@admin.register(FlowFile)
+class FlowFileAdmin(admin.ModelAdmin):
+    list_display = ['filename', 'file_date', 'record_count', 'imported_at']
+    list_filter = ['file_date', 'imported_at']
+    search_fields = ['filename']
+    readonly_fields = ['imported_at']
+
+
+@admin.register(MeterPoint)
+class MeterPointAdmin(admin.ModelAdmin):
+    list_display = ['mpan', 'meter_type', 'created_at']
+    list_filter = ['meter_type']
+    search_fields = ['mpan']
+
+
+@admin.register(Meter)
+class MeterAdmin(admin.ModelAdmin):
+    list_display = ['serial_number', 'meter_point', 'meter_type', 'installed_date']
+    list_filter = ['meter_type', 'installed_date']
+    search_fields = ['serial_number', 'meter_point__mpan']
+
+
+@admin.register(Reading)
+class ReadingAdmin(admin.ModelAdmin):
+    list_display = ['meter', 'reading_date', 'reading_value', 'register_id', 'md_flag']
+    list_filter = ['reading_date', 'md_flag']
+    search_fields = ['meter__serial_number', 'meter__meter_point__mpan']
+    date_hierarchy = 'reading_date'
