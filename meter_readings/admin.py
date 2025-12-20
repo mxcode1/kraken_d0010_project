4317216bee0514f1a51cd66cdf434415a7af3d1c
mxcode1 <mxdevelopment@yahoo.com>
2025-12-20 09:15:00 +0100

    Add admin search and filter functionality
    Advanced filtering:
    - Date-based filters on all models
    - Search across foreign keys
    - raw_id_fields for performance
    - Logical ordering defaults

--- meter_readings/admin.py
+++ meter_readings/admin.py
@@ -7,26 +7,32 @@ class FlowFileAdmin(admin.ModelAdmin):
     list_display = ['filename', 'file_date', 'record_count', 'imported_at']
     list_filter = ['file_date', 'imported_at']
     search_fields = ['filename']
-    readonly_fields = ['imported_at']
+    readonly_fields = ['imported_at', 'record_count']
+    ordering = ['-imported_at']
 
 
 @admin.register(MeterPoint)
 class MeterPointAdmin(admin.ModelAdmin):
     list_display = ['mpan', 'meter_type', 'created_at']
-    list_filter = ['meter_type']
+    list_filter = ['meter_type', 'created_at']
     search_fields = ['mpan']
+    ordering = ['mpan']
 
 
 @admin.register(Meter)
 class MeterAdmin(admin.ModelAdmin):
-    list_display = ['serial_number', 'meter_point', 'meter_type', 'installed_date']
-    list_filter = ['meter_type', 'installed_date']
+    list_display = ['serial_number', 'meter_point', 'meter_type', 'installed_date', 'removed_date']
+    list_filter = ['meter_type', 'installed_date', 'removed_date']
     search_fields = ['serial_number', 'meter_point__mpan']
+    raw_id_fields = ['meter_point']
+    ordering = ['-installed_date']
 
 
 @admin.register(Reading)
 class ReadingAdmin(admin.ModelAdmin):
-    list_display = ['meter', 'reading_date', 'reading_value', 'register_id', 'md_flag']
-    list_filter = ['reading_date', 'md_flag']
+    list_display = ['meter', 'reading_date', 'reading_value', 'register_id', 'md_flag', 'flow_file']
+    list_filter = ['reading_date', 'md_flag', 'flow_file']
     search_fields = ['meter__serial_number', 'meter__meter_point__mpan']
+    raw_id_fields = ['meter', 'flow_file']
     date_hierarchy = 'reading_date'
+    ordering = ['-reading_date']
