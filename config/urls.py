7fb732bb95219c2b15d3f9787cb393699130ea89
mxcode1 <mxdevelopment@yahoo.com>
2025-12-21 11:15:00 +0100

    Integrate dashboard into admin sidebar
    Admin customization:
    - Custom index template
    - Testing & Debug link in sidebar
    - Icon: 🧪
    - Positioned above app list

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,10 @@ URL configuration for config.
 """
 from django.contrib import admin
 from django.urls import path, include
+from meter_readings.admin_views import testing_dashboard
 
 urlpatterns = [
+    path('admin/testing/', testing_dashboard, name='testing_dashboard'),
     path('admin/', admin.site.urls),
     path('', include('meter_readings.urls')),
 ]
